5480b4f595a4c91c96fe6af09fb27b91f6b38df7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-07-18 14:03:51 +0200

    feat(posts/0000-posts.js): add post filtering by tag

--- posts/0000-posts.js
+++ posts/0000-posts.js
@@ -1,22 +1,36 @@
-const posts = [
+let posts = [
   { title: 'State manipulation vs value calculation in programming',
     date: '2016-07-28',
-    tags: [ 'Functional programming' ]
-  }
+    tags: [ 'Functional programming' ]},
+  { title: 'State manipulation vs value calculation in programming',
+    date: '2016-07-29',
+    tags: [ 'Distributed systems' ] },
+  { title: 'State manipulation vs value calculation in programming',
+    date: '2016-07-30',
+    tags: [ 'Functional programming' ] }
 ];
 
 let moment = require('moment');
 let R = require('ramda');
-
-let buildURL = function(post) {
-  post.url = post.title.toLowerCase().replace(/[^a-zA-Z0-9 ]+/g, '').
-    replace(/\s+/g, '-') + '.html';
-  return post;
-};
 
 let formatDate = function(post) {
   post.date = moment(post.date).format('D MMM, YYYY');
   return post;
 };
 
-exports.posts = R.map(R.pipe(formatDate, buildURL))(posts);
+let buildURL = function(post) {
+  post.url = post.title.toLowerCase().replace(/[^a-zA-Z0-9 ]+/g, '').
+    replace(/ +/g, '-') + '.html';
+  return post;
+};
+
+let processPosts = R.pipe(R.sortBy(R.prop('date')), R.reverse,
+                          R.map(R.pipe(formatDate, buildURL)));
+
+let byTag = function(tag) {
+  return function(post) { return R.contains(tag, post.tags); };
+};
+
+posts = processPosts(posts);
+exports.posts = posts;
+exports.fpPosts = R.filter(byTag('Functional programming'))(posts);
