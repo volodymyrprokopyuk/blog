9deae03357b55640da508bf989eb7a2560459470
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-07-18 10:27:42 +0200

    feat(posts/0000-posts.js): add formatDate and buildURL

--- posts/0000-posts.js
+++ posts/0000-posts.js
@@ -1,13 +1,22 @@
-var posts = [
+const posts = [
   { title: 'State manipulation vs value calculation in programming',
-    date: '2016-07-17',
+    date: '2016-07-28',
     tags: [ 'Functional programming' ]
   }
 ];
 
-var getURL = function(post) {
-  return post.title.toLowerCase().replace(/\s+/g, '-') + '.html';
+let moment = require('moment');
+let R = require('ramda');
+
+let buildURL = function(post) {
+  post.url = post.title.toLowerCase().replace(/[^a-zA-Z0-9 ]+/g, '').
+    replace(/\s+/g, '-') + '.html';
+  return post;
 };
 
-exports.posts = posts;
-exports.getURL = getURL;
+let formatDate = function(post) {
+  post.date = moment(post.date).format('D MMM, YYYY');
+  return post;
+};
+
+exports.posts = R.map(R.pipe(formatDate, buildURL))(posts);
